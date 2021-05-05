ab203fe45c340218cf0082558088d8a23e4355d4
Jawher27 <79726780+Jawher27@users.noreply.github.com>
2021-05-05 01:34:42 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,87 +1,92 @@
-/**
-* @file main.c
-* @brief Testing Program.
-* @author twisted minds
-* @version 0.1
-* @date Apr 26, 2021
-*
-* Testing program for perso animation
-*
-*/
-#include <stdlib.h>
 #include <stdio.h>
+
+#include <stdlib.h>
+
 #include <SDL/SDL.h>
+
 #include <SDL/SDL_image.h>
+
+#include <SDL/SDL_ttf.h>
+
 #include <SDL/SDL_mixer.h>
-#include "perso.h"
-void  main(SDL_Surface *ecran)
-{
-    personagee pers;
 
-    SDL_Event event;
-    int continuer=1,i=0,t;
-    int k;
-       //score s;
+#include "enemie.h"
 
-    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
-    ecran=SDL_SetVideoMode(1000, 667, 32,SDL_HWSURFACE);
+int main() {
 
-  int recule=0;
-    image img,img1;
-    pers=init_perso();
-    pers=init_perso2();
-    img=init_img("background.jpg",0,0);
-    img1=init_img("vie.png",780,-50);
-//init_score( &s);
-  // afficherscore(&s,ecran  );
-    SDL_EnableKeyRepeat(10,10); 
-    
-    while (continuer)
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Surface * screen;
+  screen = SDL_SetVideoMode(1000, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+
+  SDL_WM_SetCaption("entite\t1", NULL); //titre de la fenetre
+  SDL_Rect posperso;
+
+  posperso.x = 150;
+  posperso.y = 400;
+  posperso.h = 100;
+  posperso.w = 100;
+
+  Ennemi E ;
+
+  SDL_Event event;
+  SDL_Surface * background;
+  SDL_Surface * persotest;
+  SDL_Surface * persotest1;
+
+  initEnnemi(&E);
+  int running = 1;
+  background = IMG_Load("back.png");
+  persotest = IMG_Load("persotest.png");
+
+  while (running) {
+
+    while (SDL_PollEvent( & event)) {
+
+      switch (event.type) {
+      case SDL_QUIT:
+
+        running = 0;
+        break;
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+
+        case SDLK_LEFT:
+          posperso.x -= 100;
+
+          break;
+        case SDLK_RIGHT:
+          posperso.x += 100;
+
+          break;
+          case SDLK_UP:
+          posperso.y -= 100;
+          break;
+          case SDLK_DOWN:
+          posperso.y += 30;
+          break;
+
+
+        }
+        break;
+      }
+    }
+
+    SDL_BlitSurface(background, NULL, screen, NULL);
+    SDL_BlitSurface(persotest, NULL, screen, & posperso);
+
+    deplacerIA(&E,posperso);
+    animerEnnemi(&E);
+    afficherEnnemi(E,screen);
+
+    if (collision(E.pos,posperso)==1)
     {
-         SDL_PollEvent(&event);
-       switch(event.type)
-          {
-            case SDL_QUIT:
-            continuer= 0;
-            break;
-          }
-            i=deplacerperso(&pers,i,&continuer,&recule);
-            i=deplacerperso2(&pers,i,&continuer,&recule);
-           printf("%d\n",recule);
-            if (i==3)
-            {
-            i=0;
-            }
-             SDL_Delay(100);
-//afficherscore(&s,ecran  );
-             display(ecran,img);
-             display(ecran,img1);
-            afficher_perso(ecran,pers,i,pers.positionpersonage,recule);
-            afficher_perso2(ecran,pers,i,pers.positionpersonage,recule);
-         // afficherscore(&s,ecran  );
-            SDL_Flip(ecran);
-           if (pers.positionpersonage.x>800)
-            {
-            pers.positionpersonage.x=0; 
-            
-            }
-         if (pers.positionpersonage.y<400)
-         pers.positionpersonage.y=350;
-SDL_Flip(ecran);
-  
-  }    
+    	printf("collision\n");
+      SDL_BlitSurface(persotest1, NULL, screen, & posperso);
+    }
 
-
-   for(i=0;i<=3;i++)
-    SDL_FreeSurface(pers.personage[i]);
-     for(i=0;i<=3;i++)
-    SDL_FreeSurface(pers.personage1[i]);
+    SDL_Flip(screen);
+    SDL_Delay(100);
+  }
+  TTF_Quit();
 
 }
-
-
-
-
-
-
-
